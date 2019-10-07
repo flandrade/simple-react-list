9527594335d88a55cf80193ee12d3d032edf8c66
Fernanda Andrade <fandrade@stackbuilders.com>
2019-10-07 18:50:45 -0500

    Add simple list

---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+export interface User {
+  address: {
+    city: string;
+  };
+  id: number;
+  name: string;
+  status: Status;
+}
+
+export type Status = "available" | "non-available";
+
+export type getUsers = () => Promise<User[]>;
+
+export const getUsers: getUsers = function() {
+  const url = "http://jsonplaceholder.typicode.com/users";
+  return fetch(url)
+    .then(res => res.json());
+}
